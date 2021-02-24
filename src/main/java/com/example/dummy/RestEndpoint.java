7384094a8e91b8849bac3d48d5f0f6fb2c8d70d2
Mikko Östlund <Mikko.Ostlund@netent.com>
2021-02-24 14:35:32 +0100

    Change salutation to "Hello World, from <hostname>!"

--- src/main/java/com/example/dummy/RestEndpoint.java
+++ src/main/java/com/example/dummy/RestEndpoint.java
@@ -5,13 +5,21 @@ import org.springframework.stereotype.Controller;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 @Path("/helloworld")
 @Controller
 public class RestEndpoint {
     @GET
     @Produces("text/plain")
-    public String getClichedMessage() {
-        return "Hello Istio!!!";
+    public String getClichedMessage() throws IOException {
+        final Process hostnameProcess = Runtime.getRuntime().exec("hostname");
+        final InputStream inputStream = hostnameProcess.getInputStream();
+        final InputStreamReader isr = new InputStreamReader(inputStream);
+        final String hostname = new BufferedReader(isr).readLine();
+        return "Hello World, from " + hostname + "!";
     }
 }
